230e921a3a23f232dfb8b0c18758838a60baaaf5
sjm150 <sjm150@snu.ac.kr>
2023-02-14 21:21:26 +0900

    solve boggle in dp

--- algospot/boggle/src/main.rs
+++ algospot/boggle/src/main.rs
@@ -2,6 +2,7 @@ use std::io::{self, Read};
 use std::fmt::Write;
 
 const SIZE: usize = 5;
+const MAX_LEN: usize = 10;
 
 fn main() {
     let mut buf = String::new();
@@ -33,14 +34,10 @@ fn main() {
         // logic
         'outermost: for word in words {
             write!(out, "{} ", String::from_utf8_lossy(word)).unwrap();
-            for x in 0..SIZE as isize {
-                for y in 0..SIZE as isize {
-                    if has_word(&board, x, y, word) {
+                    if has_word(&board, word) {
                         writeln!(out, "YES").unwrap();
                         continue 'outermost;
                     }
-                }
-            }
             writeln!(out, "NO").unwrap();
         }
     }
@@ -48,25 +45,62 @@ fn main() {
     print!("{out}");
 }
 
-fn has_word(board: &[[u8; SIZE]; SIZE], x: isize, y: isize, word: &[u8]) -> bool {
-    if word[0] != board[x as usize][y as usize] {
-        false
-    } else if word.len() == 1 {
-        true
-    } else {
-        for dx in -1..=1 {
-            for dy in -1..=1 {
-                if dx == 0 && dy == 0 { continue; }
-                let new_x = x + dx;
-                if new_x < 0 || SIZE <= new_x as usize { continue; }
-                let new_y = y + dy;
-                if new_y < 0 || SIZE <= new_y as usize { continue; }
+fn has_word(board: &[[u8; SIZE]; SIZE], word: &[u8]) -> bool {
+    let mut has_suffix = [[[false; MAX_LEN]; SIZE]; SIZE];
 
-                if has_word(board, new_x, new_y, &word[1..]) {
-                    return true;
+    for i in (0..word.len()).rev() {
+        compute_suffix_existence(board, &word[i..], &mut has_suffix)
+    }
+
+    for x in 0..SIZE {
+        for y in 0..SIZE {
+            if has_suffix[x][y][word.len() - 1] {
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
+fn compute_suffix_existence(board: &[[u8; SIZE]; SIZE], suffix: &[u8], result: &mut [[[bool; MAX_LEN]; SIZE]; SIZE]) {
+    for x in 0..SIZE {
+        for y in 0..SIZE {
+            if suffix[0] == board[x][y] {
+                let len = suffix.len();
+
+                if len == 1 {
+                    result[x][y][0] = true;
+                } else {
+                    for (next_x, next_y) in delta(x, y, SIZE) {
+                        if result[next_x][next_y][len - 2] == true {
+                            result[x][y][len - 1] = true;
+                        }
+                    }
                 }
             }
         }
-        false
     }
+}
+
+fn delta(x: usize, y: usize, max: usize) -> Vec<(usize, usize)> {
+    let mut next = Vec::with_capacity(8);
+
+    for dx in 0..=2 {
+        for dy in 0..=2 {
+            if dx == 1 && dy == 1 { continue; }
+            let x = (x + dx).checked_sub(1);
+            let y = (y + dy).checked_sub(1);
+
+            if let Some(x) = x {
+                if let Some(y) = y {
+                    if x < max && y < max {
+                        next.push((x, y));
+                    }
+                }
+            }
+        }
+    }
+
+    next
 }
