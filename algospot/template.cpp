144d534cc72c11e2535d1b80fc2f9f6a0f410da7
sjm150 <sjm150@snu.ac.kr>
2023-02-18 04:41:13 +0900

    add cpp template for algospot problems

---
+++ algospot/template.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    int max_case;
+    cin >> max_case;
+
+    for (int c = 0; c < max_case; c++) {
+        // logic
+    }
+}
