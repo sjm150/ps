3d0371732312ca13775ad07ffcfcda27b74bb668
sjm150 <sjm150@snu.ac.kr>
2023-07-12 23:55:35 +0900

    add Makefile and template for codeforces; todo: need revision for compile scripts

---
+++ codeforces/template.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        // logic
+    }
+}
