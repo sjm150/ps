68476aa47f56cd897e21d5fdc4ff616dadbb9a04
sjm150 <sjm150@snu.ac.kr>
2023-08-08 00:25:36 +0900

    change template

--- baekjoon/template.cpp
+++ baekjoon/template.cpp
@@ -1,15 +1,6 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    cin.tie(0);
-    ios_base::sync_with_stdio(0);
-
-    int max_case;
-    cin >> max_case;
-
-    for (int c = 0; c < max_case; c++) {
-        // logic
-    }
+    cin.tie(0); ios_base::sync_with_stdio(0);
 }
