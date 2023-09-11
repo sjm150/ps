88900d2dd03017f1b0e34aac3d55cbc9fcfc0c4b
sjm150 <sjm150@snu.ac.kr>
2023-09-12 01:54:30 +0900

    change template.cpp

--- codeforces/template.cpp
+++ codeforces/template.cpp
@@ -1,12 +1,9 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    cin.tie(0);
-    ios_base::sync_with_stdio(0);
-    int t;
-    cin >> t;
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    int t; cin >> t;
     while (t--) {
         // logic
     }
