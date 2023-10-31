7cf30966215216b22eaa2556aa0ea2081d43470a
sjm150 <sjm150@snu.ac.kr>
2023-11-01 01:22:08 +0900

    solve 2743; 9086; 11718

---
+++ baekjoon/11718/solution.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    string s;
+    while (!cin.eof()) {
+        getline(cin, s);
+        cout << s << '\n';
+    }
+}
---
+++ baekjoon/9086/solution.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    int t; cin >> t;
+    while (t--) {
+        string s; cin >> s;
+        cout << s.front() << s.back() << '\n';
+    }
+}
