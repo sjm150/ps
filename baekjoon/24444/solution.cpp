d0d523cc93e3f3ef84cf066686b812bf9ed9ea4a
sjm150 <sjm150@snu.ac.kr>
2024-03-10 23:53:53 +0900

    solve 24444; 24445; 24479; 24480

---
+++ baekjoon/24444/solution.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    int n, m, r; cin >> n >> m >> r;
+    vector<vector<int>> edges(n + 1);
+    while (m--) {
+        int u, v; cin >> u >> v;
+        edges[u].push_back(v);
+        edges[v].push_back(u);
+    }
+    for (auto &e: edges) sort(e.begin(), e.end());
+
+    vector<int> vst(n + 1, 0);
+    int v = 1;
+    queue<int> q;
+    vst[r] = v++;
+    q.push(r);
+    while (!q.empty()) {
+        int cur = q.front(); q.pop();
+        for (int nxt: edges[cur]) {
+            if (!vst[nxt]) {
+                vst[nxt] = v++;
+                q.push(nxt);
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++) cout << vst[i] << '\n';
+}
---
+++ baekjoon/24480/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    int n, m, r; cin >> n >> m >> r;
+    vector<vector<int>> edges(n + 1);
+    while (m--) {
+        int u, v; cin >> u >> v;
+        edges[u].push_back(v);
+        edges[v].push_back(u);
+    }
+    for (auto &e: edges) sort(e.begin(), e.end(), greater<int>());
+
+    vector<int> vst(n + 1, 0);
+    int v = 1;
+    function<void(int)> dfs = [&](int cur) {
+        vst[cur] = v++;
+        for (int nxt: edges[cur]) {
+            if (!vst[nxt]) dfs(nxt);
+        }
+    };
+
+    dfs(r);
+    for (int i = 1; i <= n; i++) cout << vst[i] << '\n';
+}
