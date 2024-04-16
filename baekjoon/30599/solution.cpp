d37809034df99135c9a5e572aa3603c34c9a89b1
sjm150 <sjm150@snu.ac.kr>
2024-04-16 23:27:42 +0900

    solve 30599

---
+++ baekjoon/30599/solution.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    int d; cin >> d;
+    for (int i = 0; i < d; i++) cout << d;
+    cout << '\n';
+}
