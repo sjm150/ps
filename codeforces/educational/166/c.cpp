4bfb1ed6a243088d44063dbec1b01b5ae84d514a
sjm150 <sjm150@snu.ac.kr>
2024-06-03 00:07:54 +0900

    update c

--- codeforces/educational/166/c.cpp
+++ codeforces/educational/166/c.cpp
@@ -11,49 +11,28 @@ int main() {
         for (auto &ab: ab) cin >> ab[0];
         for (auto &ab: ab) cin >> ab[1];
 
-        vector<int> pst(sz), mis;
-        pst[0] = ab[0][0] < ab[0][1];
+        vector<int> pst(sz);
+        int cnt[] = {0, 0};
+        int mis = -1;
         long long val = 0;
-        int cnt[2] = {0, 0}, lst = 0;
 
-        for (int i = 1; i < sz; i++) {
-            if (ab[i][0] > ab[i][1]) {
-                if (cnt[0] < nm[0]) {
-                    pst[i] = 0;
-                    val += ab[i][0];
-                    cnt[0]++;
-                } else {
-                    pst[i] = 1;
-                    val += ab[i][1];
-                    if (pst[0] == 1) mis.push_back(i);
-                }
-            } else {
-                if (cnt[1] < nm[1]) {
-                    pst[i] = 1;
-                    val += ab[i][1];
-                    cnt[1]++;
-                } else {
-                    pst[i] = 0;
-                    val += ab[i][0];
-                    if (pst[0] == 0) mis.push_back(i);
-                }
+        for (int i = 0; i < sz - 1; i++) {
+            int p = ab[i][0] < ab[i][1];
+            if (cnt[p] == nm[p]) {
+                p = 1 - p;
+                if (mis < 0) mis = i;
             }
-            if (pst[0] == pst[i]) lst = i;
+            pst[i] = p;
+            cnt[p]++;
+            val += ab[i][p];
         }
 
-        cout << val << ' ';
-        val += ab[0][pst[0]];
-        for (int i = 1; i < sz; i++) {
+        for (int i = 0; i < sz - 1; i++) {
             val -= ab[i][pst[i]];
-            if (pst[0] == pst[i]) {
-                cout << val << ' ';
-            } else {
-                auto it = upper_bound(mis.begin(), mis.end(), i);
-                if (it != mis.end()) cout << val - ab[*it][pst[0]] + ab[*it][pst[i]] << ' ';
-                else cout << val - ab[lst][pst[0]] + ab[lst][pst[i]] << ' ';
-            }
+            if (mis >= 0 && i < mis && pst[i] != pst[mis]) cout << val - ab[mis][pst[mis]] + ab[mis][1 - pst[mis]] + ab[sz - 1][pst[mis]] << ' ';
+            else cout << val + ab[sz - 1][pst[i]] << ' ';
             val += ab[i][pst[i]];
         }
-        cout << '\n';
+        cout << val << '\n';
     }
 }
