9c2eb4d859d9f9f459abd3b7e2d0c590239d0fb4
sjm150 <sjm150@snu.ac.kr>
2024-08-12 10:44:09 +0900

    update c

--- codeforces/etc/epic24august/c.cpp
+++ codeforces/etc/epic24august/c.cpp
@@ -12,31 +12,14 @@ int main() {
         for (auto &[x, y]: xy) cin >> x >> y;
         int xs, ys, xt, yt; cin >> xs >> ys >> xt >> yt;
         int dx = xt - xs, dy = yt - ys;
-
-        auto cross = [&](ll x1, ll y1, ll x2, ll y2) {
-            return x1 * y2 - x2 * y1;
-        };
-        auto ccw = [&](ll x1, ll y1, ll x2, ll y2) {
-            ll c = cross(x1, y1, x2, y2);
-            if (c > 0) return 1;
-            else if (c < 0) return -1;
-            else return 0;
-        };
         auto sqr = [&](ll x) { return x * x; };
-
         bool poss = true;
         for (auto [x, y]: xy) {
             if (sqr(xt - x) + sqr(yt - y) <= sqr(dx) + sqr(dy)) {
                 poss = false;
                 break;
             }
-            if (ccw(-dy, dx, xs - x, ys - y) * ccw(-dy, dx, xt - x, yt - y) < 0 &&
-                abs(dy * ll(x - xs) - dx * ll(y - ys)) <= abs(dx * ll(xs - x) + dy * ll(ys - y))) {
-                poss = false;
-                break;
-            }
         }
-
         cout << (poss ? "YES\n" : "NO\n");
     }
 }
