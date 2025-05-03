0ee809114d8092b555a592bb7500b25715568d88
sjm150 <sjm150@snu.ac.kr>
2025-05-04 00:04:28 +0900

    solve 11479

---
+++ baekjoon/11479/solution.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    string s; cin >> s;
+    int n = s.size();
+
+    vector<int> sa(n), pos(n);
+    for (int i = 0; i < n; i++) {
+        sa[i] = i;
+        pos[i] = s[i];
+    }
+    for (int d = 1; ; d *= 2) {
+        auto cmp = [&](int a, int b) {
+            if (pos[a] != pos[b]) return pos[a] < pos[b];
+            a += d, b += d;
+            return a < n && b < n ? pos[a] < pos[b] : a > b;
+        };
+        auto np = pos;
+        sort(sa.begin(), sa.end(), cmp);
+        np[sa[0]] = 0;
+        for (int i = 1; i < n; i++) np[sa[i]] = np[sa[i - 1]] + cmp(sa[i - 1], sa[i]);
+        swap(np, pos);
+        if (pos[sa[n - 1]] == n - 1) break;
+    }
+
+    vector<int> lcp(n - 1);
+    for (int i = 0, k = 0; i < n; i++, k = max(k - 1, 0)) {
+        if (pos[i] == n - 1) continue;
+        for (int j = sa[pos[i] + 1]; max(i, j) + k < n && s[i + k] == s[j + k];) k++;
+        lcp[pos[i]] = k;
+    }
+
+    cout << (long long)(n + 1) * n / 2 - accumulate(lcp.begin(), lcp.end(), 0ll) << '\n';
+}
