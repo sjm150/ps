5ac4d4d5aa63ff4c7c6fe7d892c8d509deaf6967
sjm150 <sjm150@snu.ac.kr>
2025-05-21 01:01:15 +0900

    change solution

--- baekjoon/25378/solution.cpp
+++ baekjoon/25378/solution.cpp
@@ -3,31 +3,34 @@ using namespace std;
 
 int main() {
     cin.tie(0); ios_base::sync_with_stdio(0);
+    typedef pair<int, int> pi;
     int n;
     cin >> n;
     vector<int> a(n);
     for (int &a: a) cin >> a;
 
-    map<int, int> pre;
-    pre[0] = 0;
-    for (int a: a) {
-        map<int, int> cur;
-        auto asgn = [&](int k, int v) {
-            auto it = cur.find(k);
-            if (it == cur.end()) cur[k] = v;
-            else it->second = min(it->second, v);
-        };
-        for (auto [p, v]: pre) {
-            asgn(a, v + (p > 0));
-            if (p <= a) asgn(a - p, v + 1);
+    vector<pi> p;
+    for (int i = 0; i < n; i++) {
+        int r = a[i];
+        for (int j = i + 1; j < n; j++) {
+            if (r > a[j]) {
+                break;
+            } else if (r < a[j]) {
+                r = a[j] - r;
+            } else {
+                p.emplace_back(i, j);
+                break;
+            }
         }
-        swap(pre, cur);
     }
+    sort(p.begin(), p.end(), [](pi &a, pi &b) { return a.second < b.second; });
 
-    int ans = n;
-    for (auto [p, v]: pre) {
-        if (p) ans = min(ans, v + 1);
-        else ans = min(ans, v);
+    int ans = n, lst = -1;
+    for (auto [s, e]: p) {
+        if (lst < s) {
+            ans--;
+            lst = e;
+        }
     }
     cout << ans << '\n';
 }
