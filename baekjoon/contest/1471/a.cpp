5321f3732d8e3d7296ad9ea4097751e09b68efe3
sjm150 <sjm150@snu.ac.kr>
2025-05-26 00:16:04 +0900

    update a

--- baekjoon/contest/1471/a.cpp
+++ baekjoon/contest/1471/a.cpp
@@ -5,12 +5,5 @@ int main() {
     cin.tie(0); ios_base::sync_with_stdio(0);
     int n;
     cin >> n;
-
-    auto solve = [&]() {
-        int c = (n - 1) * 2;
-        int f = (n - 2) * (n - 1) / 2, b = (n - 1) * n / 2;
-        return 1 * 35 + c * 28 + f * 21 + b * 14;
-    };
-
-    cout << solve() << '\n';
+    cout << n * (n + 1) / 2 * 35 << '\n';
 }
