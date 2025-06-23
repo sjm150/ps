9d62e0ef6b5c5f466018ae9973901f890f90801f
sjm150 <sjm150@snu.ac.kr>
2025-06-24 02:09:39 +0900

    solve 10953

---
+++ baekjoon/10953/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b;
+        char c;
+        cin >> a >> c >> b;
+        cout << a + b << '\n';
+    }
+}
