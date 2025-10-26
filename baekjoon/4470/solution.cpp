da075e3cf3c5e192e6c4ff3eb2b2a7811ef146ff
sjm150 <sjm150@snu.ac.kr>
2025-10-27 01:31:27 +0900

    solve 4470

---
+++ baekjoon/4470/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    int n;
+    cin >> n;
+    cin.ignore();
+    for (int i = 1; i <= n; i++) {
+        string s;
+        getline(cin, s);
+        cout << i << ". " << s << '\n';
+    }
+}
