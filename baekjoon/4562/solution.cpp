c2f42d9f731110d155bc6d3f7b1906e924c19bd3
sjm150 <sjm150@snu.ac.kr>
2025-11-01 03:10:36 +0900

    solve 4562

---
+++ baekjoon/4562/solution.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    int n;
+    cin >> n;
+    while (n--) {
+        int x, y;
+        cin >> x >> y;
+        cout << (x < y ? "NO" : "MMM") << " BRAINS\n";
+    }
+}
