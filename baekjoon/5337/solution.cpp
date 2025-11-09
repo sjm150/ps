edee79d253692a9a5b0caf62f0f26befcfae891d
sjm150 <sjm150@snu.ac.kr>
2025-11-10 02:48:52 +0900

    solve 5337

---
+++ baekjoon/5337/solution.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    cout << ".  .   .\n";
+    cout << "|  | _ | _. _ ._ _  _\n";
+    cout << "|/\\|(/.|(_.(_)[ | )(/.\n";
+}
