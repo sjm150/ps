eb36ef5384283121f44f9afae53bc8310a8cda52
sjm150 <sjm150@snu.ac.kr>
2025-12-06 02:08:46 +0900

    solve 6778

---
+++ baekjoon/6778/solution.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    int a, b;
+    cin >> a >> b;
+    if (a >= 3 && b <= 4) cout << "TroyMartian\n";
+    if (a <= 6 && b >= 2) cout << "VladSaturnian\n";
+    if (a <= 2 && b <= 3) cout << "GraemeMercurian\n";
+}
