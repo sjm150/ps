5d1e696682274ba10015f5445b69ceb4df2dd4e2
sjm150 <sjm150@snu.ac.kr>
2025-12-09 02:18:25 +0900

    solve 6810

---
+++ baekjoon/6810/solution.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << "The 1-3-sum is " << 91 + a + b * 3 + c << '\n';
+}
