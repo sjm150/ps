443be944798d4daa22d93c30a3b2a9d13fd8ec2d
sjm150 <sjm150@snu.ac.kr>
2026-01-06 01:19:01 +0900

    solve 13752

---
+++ baekjoon/13752/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    int n;
+    cin >> n;
+    while (n--) {
+        int k;
+        cin >> k;
+        while (k--) cout << '=';
+        cout << '\n';
+    }
+}
