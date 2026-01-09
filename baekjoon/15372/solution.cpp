52f6bec776e7b3b0cde5d20aabb66985b0671ebd
sjm150 <sjm150@snu.ac.kr>
2026-01-10 01:15:33 +0900

    solve 15372

---
+++ baekjoon/15372/solution.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << (long long)(n) * n << '\n';
+    }
+}
