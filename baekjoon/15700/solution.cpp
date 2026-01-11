84d5a7139a3b0bd0bb432915ab56743e2cf70856
sjm150 <sjm150@snu.ac.kr>
2026-01-12 01:40:11 +0900

    solve 15700

---
+++ baekjoon/15700/solution.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    int n, m;
+    cin >> n >> m;
+    cout << (long long)(n) * m / 2 << '\n';
+}
