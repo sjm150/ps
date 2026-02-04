3833a6f833692bf0f364112408bef59c580cf575
sjm150 <sjm150@snu.ac.kr>
2026-02-05 00:15:41 +0900

    solve 16478

---
+++ baekjoon/16478/solution.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << setprecision(7) << double(a) * c / b << '\n';
+}
