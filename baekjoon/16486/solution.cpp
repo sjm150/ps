010c440f2bd62fec8fc462fda57adf88b38d49fd
sjm150 <sjm150@snu.ac.kr>
2026-02-23 00:05:04 +0900

    solve 16486

---
+++ baekjoon/16486/solution.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    int a, b;
+    cin >> a >> b;
+    cout << setprecision(7) << fixed << a * 2 + b * 2 * M_PI << '\n';
+}
