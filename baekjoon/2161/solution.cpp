f01815574a6297893f1926d053d34b7add412da9
sjm150 <sjm150@snu.ac.kr>
2026-02-22 23:19:42 +0900

    solve 2161

---
+++ baekjoon/2161/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    int n;
+    cin >> n;
+    queue<int> q;
+    for (int i = 1; i <= n; i++) q.push(i);
+    while (!q.empty()) {
+        cout << q.front() << ' ';
+        q.pop();
+        if (!q.empty()) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    cout << '\n';
+}
